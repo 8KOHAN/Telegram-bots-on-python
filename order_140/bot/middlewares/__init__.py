543e36509835a975f876044b544aa0645356a6ac
Alexey Kohan <kohanzom@gmail.com>
2025-05-21 13:14:11 +0300

    Create __init__.py

---
+++ order_140/bot/middlewares/__init__.py
@@ -0,0 +1,14 @@
+from aiogram import Dispatcher
+
+from .admin import AdminMiddleware
+from aiogram_album.no_check_count_middleware import WithoutCountCheckAlbumMiddleware
+
+def setup_middlewares(dispatcher: Dispatcher) -> None:
+    WithoutCountCheckAlbumMiddleware(router=dispatcher)
+    dispatcher.message.outer_middleware(AdminMiddleware())
+    dispatcher.callback_query.outer_middleware(AdminMiddleware())
+
+
+__all__ = [
+    'setup_middlewares'
+]
