33e6030486f128602dbdeb39f7faadc9477138f6
Alexey Kohan <kohanzom@gmail.com>
2025-07-17 18:03:27 +0300

    Delete order_140/bot/fsm/states/super_admin.py

--- order_140/bot/fsm/states/super_admin.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class SuperAdminStates(StatesGroup):
-
-    add_admin_enter_id = State()
-    process_admin_statistics = State()
-    process_channel_statistics = State()
