300093025d55c0113a557497493dd3fbf9dce2a1
Alexey Kohan <kohanzom@gmail.com>
2025-07-17 18:02:09 +0300

    Delete order_140/bot/core/database/base.py

--- order_140/bot/core/database/base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    def __repr__(self) -> str:
-        body = ""
-        return f"{self.__class__.__name__}[\n{body}\n"
